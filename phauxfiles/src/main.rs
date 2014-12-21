72b98c286f87805b7487e43a31a77ca81480799a
John Colagioia <john@colagioia.net>
2014-12-21 06:29:17 -0500

    Generalize HTTP request and move to function

--- phauxfiles/src/main.rs
+++ phauxfiles/src/main.rs
@@ -1,4 +1,14 @@
+extern crate serialize;
+use serialize::json;
 use std::str;
+
+#[deriving(Decodable, Encodable)]
+pub struct FauxPerson {
+    name: String,
+    surname: String,
+    gender: String,
+    country: String,
+}
 
 fn main() {
     let mut sock_names = std::io::TcpStream::connect("api.uinames.com:80").unwrap();
@@ -25,6 +35,9 @@ fn main() {
     let parts: Vec<&str> = http_pieces.collect();
     let names_body = parts[parts.len() - 1];
 
-    println!("{}\n", names_body);
+    let people: Vec<FauxPerson> = json::decode(names_body.as_slice()).unwrap();
+    for who in people.iter() {
+        println!("{} {}\n{} from {}\n", who.name, who.surname, who.gender, who.country);
+    }
 }
 
