f3497ec0a0668d92932311e5bcc75d622f7fb767
John Colagioia <john@colagioia.net>
2014-12-26 06:46:22 -0500

    Move data structures to separate module

---
+++ phauxfiles/src/fauxperson.rs
@@ -0,0 +1,41 @@
+use std::fmt;
+
+#[deriving(Decodable, Encodable)]
+pub struct FauxPerson {
+    name: String,
+    surname: String,
+    gender: String,
+    country: String,
+}
+
+impl fmt::Show for FauxPerson {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let (ref name, ref surname) = match self.country.as_slice() {
+            // Surnames still come first for Chinese names, presumably
+            "China" => (self.surname.clone(), self.name.clone()),
+            _ => (self.name.clone(), self.surname.clone()),
+        };
+        write!(f, "{} {}\n{} from {}", name, surname, self.gender, self.country)
+    }
+}
+
+#[deriving(Decodable, Encodable)]
+pub struct ImageUrl {
+    epic: String,
+    bigger: String,
+    normal: String,
+    mini: String,
+}
+
+#[deriving(Decodable, Encodable)]
+pub struct FaceCollection {
+    username: String,
+    image_urls: ImageUrl,
+}
+
+impl fmt::Show for FaceCollection {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.image_urls.epic)
+    }
+}
+
