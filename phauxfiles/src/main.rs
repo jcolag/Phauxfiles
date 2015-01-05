27651bcb3a83817752956d656f89d304b5ceb2d8
John Colagioia <john@colagioia.net>
2015-01-05 06:32:38 -0500

    Change print statements to more flexible output

--- phauxfiles/src/main.rs
+++ phauxfiles/src/main.rs
@@ -5,26 +5,36 @@ use std::str;
 use fauxperson::{FauxPerson,FaceCollection};
 
 mod fauxperson;
+mod outfile;
 
 fn main() {
     let args = os::args();
     let mut count = "6";
+    let mut out: outfile::FileIo;
+
     if args.len() > 1 {
         count = args[1].as_slice();
     }
+    out = if args.len() > 2 {
+        outfile::FileIo::new(args[2].to_string())
+    } else {
+        outfile::FileIo::new("".to_string())
+    };
+
     let path = format!("/?amount={}", count);
     let names = http_get("api.uinames.com", 80, path.as_slice());
     let people: Vec<FauxPerson> = json::decode(names.as_slice()).unwrap();
-    println!("<html><head><title>Fake Search Results</title>");
-    println!("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
-    println!("<link rel='stylesheet' href='format.css'");
-    println!("</head><body>");
+    out.write("<html><head><title>Fake Search Results</title>");
+    out.write("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
+    out.write("<link rel='stylesheet' href='format.css'");
+    out.write("</head><body>");
     for who in people.iter() {
         let faces = http_get("uifaces.com", 80, "/api/v1/random");
         let urls: FaceCollection = json::decode(faces.as_slice()).unwrap();
-        println!("<div class='profile'>\n{}\n{}\n</div>\n", urls.to_string(), who.to_string());
+        let div = format!("<div class='profile'>\n{}\n{}\n</div>\n", urls.to_string(), who.to_string());
+        out.write(div.as_slice());
     }
-    println!("</body></html>")
+    out.write("</body></html>");
 }
 
 fn http_get(host: &str, port: i32, path: &str) -> String {
