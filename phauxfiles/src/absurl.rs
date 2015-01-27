62682864877fc6dfa340966b7db9a249c988146e
John Colagioia <john@colagioia.net>
2015-01-27 06:35:07 -0500

    Extract URI parser

---
+++ phauxfiles/src/absurl.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+pub struct AbsUrl {
+    pub path: String,
+    pub args: HashMap<String, String>,
+}
+
+impl AbsUrl {
+    pub fn new(path: &String) -> AbsUrl {
+        let mut args: HashMap<String, String> = HashMap::new();
+        let mut path_parts = path.split_str("?");
+        let out_path = path_parts.next().unwrap().to_string();
+        let query = match path_parts.next() {
+            Some(s) => s.to_string(),
+            None => "".to_string(),
+        };
+
+        for var in query.split_str("&") {
+            let mut var_parts = var.split_str("=");
+            let key = match var_parts.next() {
+                Some(s) => s.to_string(),
+                None => continue,
+            };
+            let value = match var_parts.next() {
+                Some(s) => s.to_string(),
+                None => "".to_string(),
+            };
+            if !key.is_empty() {
+                args.insert(key, value);
+            }
+        }
+
+        AbsUrl {
+            path: out_path,
+            args: args,
+        }
+    }
+}
